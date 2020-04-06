85208879af9aa1f57508c7978ae8d4f68630371a
shahinkl <shahin@saal.ai>
2020-04-06 21:25:30 +0400

    Added egs/prn configs

--- open_shell.sh
+++ open_shell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 vols="-v ${PWD}/egs:/espnet/egs -v ${PWD}/espnet:/espnet/espnet -v ${PWD}/test:/espnet/test -v ${PWD}/utils:/espnet/utils"
-docker run vols -it espnet/espnet:cpu-18 /bin/bash
+docker run "$vols" -it espnet/espnet:cpu-18 /bin/bash

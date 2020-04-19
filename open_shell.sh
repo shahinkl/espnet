656a20bd4ab4ac8b9d0fa303e5f2b69e8628987f
Shahin Konadath <shahin@kL-ML2.kryptolabs.com>
2020-04-19 10:29:04 +0400

    local changes

--- open_shell.sh
+++ open_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 vols="-v ${PWD}/egs:/espnet/egs -v ${PWD}/espnet:/espnet/espnet -v ${PWD}/test:/espnet/test -v ${PWD}/utils:/espnet/utils"
 # shellcheck disable=SC2086
-docker run $vols -it espnet/espnet:cpu-u18 /bin/bash
+docker run $vols -it espnet/espnet:gpu-cuda10.1-cudnn7-u18 /bin/bash

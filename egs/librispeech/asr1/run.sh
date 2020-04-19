56d2112a4bbd601cf7cd994755702af583b2b528
shahinkl <shahin@saal.ai>
2020-04-19 19:12:59 +0400

    Merging changes from remote

--- egs/librispeech/asr1/run.sh
+++ egs/librispeech/asr1/run.sh
@@ -46,7 +46,7 @@ use_lm_valbest_average=false # if true, the validation `lm_n_average`-best langu
 # Set this to somewhere where you want to put your data, or where
 # someone else has already put it.  You'll want to change this
 # if you're not on the CLSP grid.
-datadir=/export/a15/vpanayotov/data
+datadir=downloads
 
 # base url for downloads.
 data_url=www.openslr.org/resources/12

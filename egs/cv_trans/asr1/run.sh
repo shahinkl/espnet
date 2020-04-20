5bd46b1708158076e188acc432f57464ea6133b2
shahinkl <shahin@saal.ai>
2020-04-20 10:27:14 +0400

    Merging changes from remote

--- egs/cv_trans/asr1/run.sh
+++ egs/cv_trans/asr1/run.sh
@@ -15,7 +15,7 @@ debugmode=1
 dumpdir=dump   # directory to dump full features
 N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
 verbose=0      # verbose option
-resume=        # Resume the training from snapshot
+resume="snapshot.ep.15"        # Resume the training from snapshot
 
 # feature configuration
 do_delta=false

c130b12315257a582db033da7447a33afb413025
shahinkl <shahin@saal.ai>
2020-04-23 00:10:27 +0400

    Merging changes from remote

--- egs/ai-all/asr1/run.sh
+++ egs/ai-all/asr1/run.sh
@@ -259,7 +259,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 #    utils/fix_data_dir.sh ${data_dir}/${x}
 #  done
 
-  utils/combine_data.sh skip_fix true --extra_files utt2num_frames ${train_set_org} "${data_dir}/ami/ihm_train" \
+  utils/combine_data.sh --skip_fix true --extra_files utt2num_frames ${train_set_org} "${data_dir}/ami/ihm_train" \
     "${data_dir}/commonvoice/train" \
     "${data_dir}/dipco/dev_worn" \
     "${data_dir}/dipco/eval_worn" \
@@ -270,7 +270,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     "${data_dir}/tedlium3/train" \
     "${data_dir}/voxforge/all_en"
 
-  utils/combine_data.sh skip_fix true --extra_files utt2num_frames ${dev_set_org} "${data_dir}/ami/ihm_dev" \
+  utils/combine_data.sh --skip_fix true --extra_files utt2num_frames ${dev_set_org} "${data_dir}/ami/ihm_dev" \
     "${data_dir}/commonvoice/dev" \
     "${data_dir}/dipco/dev_beamformit_ref" \
     "${data_dir}/librispeech/dev_clean" \
@@ -278,7 +278,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     "${data_dir}/tedlium2/dev" \
     "${data_dir}/tedlium3/dev"
 
-  utils/combine_data.sh skip_fix true --extra_files utt2num_frames ${test_set} "${data_dir}/ami/ihm_eval" \
+  utils/combine_data.sh --skip_fix true --extra_files utt2num_frames ${test_set} "${data_dir}/ami/ihm_eval" \
     "${data_dir}/commonvoice/test" \
     "${data_dir}/dipco/eval_beamformit_ref" \
     "${data_dir}/librispeech/test_clean" \

2f7c040e4386140d533c4ddd469fd75c9d7c86ad
shahinkl <shahin@saal.ai>
2020-04-23 10:09:42 +0400

    Merging changes from remote

--- docker/run_monitor.sh
+++ docker/run_monitor.sh
@@ -3,4 +3,8 @@
 egs=$1
 event_dir=$2
 cmd="tensorboard --logdir=/tmp/egs/$egs/asr1/tensorboard/$event_dir --host=0.0.0.0"
+docker_image=$(docker images -q tensorboad:latest)
+if ! [[ -n ${docker_image} ]]; then
+  docker build -f prebuilt/monitor/Dockerfile -t tensorboad:latest
+fi
 docker run --name tensorboard -v /store/workspace/saal/espnet:/tmp -p 6006:6006 -i --rm tensorboad /bin/bash -c "$cmd"
---
+++ egs/ai-all/asr1/monitor_lm.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../../../docker/run_monitor.sh ai-all train_rnnlm_pytorch_lm_unigram5000_ngpu8

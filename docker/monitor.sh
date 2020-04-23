01d3d1bff97c217bcc9664d5c1bae5bced88e71a
shahinkl <shahin@saal.ai>
2020-04-23 09:39:30 +0400

    Merging changes from remote

--- docker/monitor.sh
+++ docker/monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-egs=cv_trans
-event_dir=valid_train_en_pytorch_train
+
+egs=$1
+event_dir=$2
 cmd="tensorboard --logdir=/tmp/egs/$egs/asr1/tensorboard/$event_dir --host=0.0.0.0"
 docker run --name tensorboard -v /store/workspace/saal/espnet:/tmp -p 6006:6006 -i --rm tensorboad /bin/bash -c "$cmd"

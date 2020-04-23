085c9a62ddba71582fef1bf43121d5124895d9cf
shahinkl <shahin@saal.ai>
2020-04-23 12:17:06 +0400

    Merging changes from remote

--- egs/ai-all/asr1/local/tedlium3/prepare_data.sh
+++ egs/ai-all/asr1/local/tedlium3/prepare_data.sh
@@ -7,4 +7,5 @@ data_type=$3
 local/tedlium3/prepare_ted_data.sh "${dwl_dir}" "${data_dir}" "${data_type}"
 for dset in dev test train; do
   utils/data/modify_speaker_info.sh --seconds-per-spk-max 180 "${data_dir}/${dset}.orig" "${data_dir}/${dset}"
+  utils/fix_data_dir.sh "${data_dir}/${dset}"
 done
